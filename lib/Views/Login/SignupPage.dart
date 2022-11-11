4a588b37d3553931e9a88aacc042711e0d3750b0
Dipisha9538 <78410665+Dipisha9538@users.noreply.github.com>
2022-11-11 09:10:00 +0545

    widget and notification

--- lib/Views/Login/SignupPage.dart
+++ lib/Views/Login/SignupPage.dart
@@ -2,6 +2,7 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:pasell/Widgets/TextFormFrave.dart';
 import '../../Widgets/CustomText.dart';
 import '../../Widgets/CustomButton.dart';
 
@@ -26,128 +27,122 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      child: Scaffold(
+    child:
+    Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
-              color: Color.fromARGB(255, 15, 11, 11),
-            ),
-            onPressed: () => Navigator.pop(context),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Color.fromARGB(255, 15, 11, 11),
           ),
-          actions: [
-            TextButton(
-              child:
-                  CustomText(text: 'SignIn', fontSize: 18, color: Colors.white),
-              onPressed: () =>
-                  Navigator.of(context).pushReplacementNamed('signInPage'),
-            ),
-            SizedBox(width: 5)
-          ],
+          onPressed: () => Navigator.pop(context),
         ),
-        body: Form(
-          key: _formKey,
-          child: ListView(
-            // padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-            physics: BouncingScrollPhysics(),
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    child: Image.asset(
-                      'Assets/logoo.png',
-                      height: 40,
-                    ),
+        actions: [
+          TextButton(
+            child:
+                CustomText(text: 'SignIn', fontSize: 18, color: Colors.white),
+            onPressed: () =>
+                Navigator.of(context).pushReplacementNamed('signInPage'),
+          ),
+          SizedBox(width: 5)
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          // padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          physics: BouncingScrollPhysics(),
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  child: Image.asset(
+                    'Assets/logoo.png',
+                    height: 40,
                   ),
-                ],
-              ),
-              // Row(),
-              SizedBox(height: 15.0),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20),
-                child: CustomText(
-                  text: 'Sign Up',
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 25, 175, 202),
                 ),
+              ],
+            ),
+            // Row(),
+            SizedBox(height: 15.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: CustomText(
+                text: 'Sign Up',
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 25, 175, 202),
               ),
+            ),
 
-              // Padding(padding: EdgeInsets.only(right: 120)),
-              // SizedBox(height: 20.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 10.0),
-                child: TextFormFrave(
-                  hintText: 'Username',
-                  fontSize: 18,
-                  prefixIcon: Icons.person_outline_rounded,
-                  controller: userController,
-                  validator:
-                      RequiredValidator(errorText: 'Username is required'),
-                ),
+            // Padding(padding: EdgeInsets.only(right: 120)),
+            // SizedBox(height: 20.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: TextFormFrave(
+                hintText: 'Username',
+                fontSize: 18,
+                prefixIcon: Icons.person_outline_rounded,
+                controller: userController,
+                validator: RequiredValidator(errorText: 'Username is required'),
               ),
-              // SizedBox(height: 15.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 10.0),
-                child: TextFormFrave(
-                    hintText: 'Email Address',
-                    type: TextInputType.emailAddress,
-                    prefixIcon: Icons.email_outlined,
-                    controller: emailController,
-                    validator: validatedEmail),
+            ),
+            // SizedBox(height: 15.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: TextFormFrave(
+                hintText: 'Email Address',
+                type: TextInputType.emailAddress,
+                prefixIcon: Icons.email_outlined,
+                controller: emailController,
               ),
-              // SizedBox(height: 15.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 10.0),
-                child: TextFormFrave(
-                  hintText: 'Password',
+            ),
+            // SizedBox(height: 15.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: TextFormFrave(
+                hintText: 'Password',
+                prefixIcon: Icons.vpn_key_rounded,
+                obscureText: true,
+                controller: passowrdController,
+              ),
+            ),
+            // SizedBox(height: 15.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: TextFormFrave(
+                  hintText: 'Repeat Password',
+                  controller: passController,
                   prefixIcon: Icons.vpn_key_rounded,
                   obscureText: true,
-                  controller: passowrdController,
-                  validator: passwordValidator,
-                ),
-              ),
-              // SizedBox(height: 15.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 10.0),
-                child: TextFormFrave(
-                    hintText: 'Repeat Password',
-                    controller: passController,
-                    prefixIcon: Icons.vpn_key_rounded,
-                    obscureText: true,
-                    validator: (val) =>
-                        MatchValidator(errorText: 'Password do not macth ')
-                            .validateMatch(val, passowrdController.text)),
-              ),
-              // SizedBox(height: 25.0),
-              // SizedBox(height: 25.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 15.0, vertical: 10.0),
-                child: CustomButton(
+                  validator: (val) =>
+                      MatchValidator(errorText: 'Password do not macth ')
+                          .validateMatch(val, passowrdController.text)),
+            ),
+            // SizedBox(height: 25.0),
+            // SizedBox(height: 25.0),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+              child: CustomButton(
                   text: 'Sign Up',
                   fontWeight: FontWeight.w500,
                   fontSize: 20,
                   height: 60,
-                  onPressed: () {
-                    
-                    }
-                  
-                ),
-              )
-            ],
-          ),
+                  onPressed: () {}),
+            )
+          ],
         ),
-      )
-    
+      ),
+    );
   }
 }
