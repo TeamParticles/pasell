f886e9edd3663ed20c6f0d4855c8ff89e87a92d3
Dipisha9538 <78410665+Dipisha9538@users.noreply.github.com>
2022-11-20 08:53:43 +0545

    HomeScreen widget

---
+++ lib/Views/Start/HomeScreenPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 150, 227, 248),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
