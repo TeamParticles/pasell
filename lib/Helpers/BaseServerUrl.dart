4919c744c8b72b84ab728026498878367f69781d
Dipisha9538 <78410665+Dipisha9538@users.noreply.github.com>
2022-11-20 21:30:46 +0545

    Helpers

---
+++ lib/Helpers/BaseServerUrl.dart
@@ -0,0 +1,8 @@
+// const baseServerUrl = "http://10.1.7.122:8471/api";
+// const publicServerUrl = "http://10.1.7.122:8471/";
+
+const baseServerUrl = "http://192.168.1.67:8471/api";
+const publicServerUrl = "http://192.168.1.67::8471/";
+
+// const baseServerUrl = "http://192.168.1.67:8471/api";
+// const publicServerUrl = "http://192.168.1.67:8471/";
---
+++ lib/Helpers/Colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color.fromARGB(255, 88, 88, 86);
+Color white = Color.fromARGB(255, 248, 154, 154);
