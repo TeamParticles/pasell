36630041a3fa48c6825b62cc5298dd25ab906e55
Dipisha9538 <78410665+Dipisha9538@users.noreply.github.com>
2022-11-20 08:54:38 +0545

    HomeScreen listview

--- lib/Views/Start/HomeScreenPage.dart
+++ lib/Views/Start/HomeScreenPage.dart
@@ -10,7 +10,48 @@ class HomeScreenPage extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 15.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: [
+              Expanded(
+                flex: 4,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            child: Image.asset(
+                              'Assets/logoo.png',
+                              height: 100,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: ListView(
+                    physics: BouncingScrollPhysics(),
+                    children: [
+                      FloatingActionButton(
+                        child: Icon(Icons.arrow_forward_ios),
+                        backgroundColor: Color.fromARGB(211, 93, 79, 247),
+                        foregroundColor: Colors.white,
+                        onPressed: () => {
+                          Navigator.of(context).pushNamed('HomeScreenPageee')
+                        },
+                      ),
+                      SizedBox(height: 10.0),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
