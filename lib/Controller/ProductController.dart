dfbdcd982b18ef7b260b42bacd6603872b6e39aa
78891RAM <raambhandari78891@gmail.com>
2022-12-07 22:20:08 +0545

    product controller

---
+++ lib/Controller/ProductController.dart
@@ -0,0 +1,119 @@
+import 'dart:convert';
+import 'package:pasell/Helpers/BaseServerUrl.dart';
+import 'package:pasell/Models/AuthModel.dart';
+import 'package:pasell/Models/Home/ProductHome.dart';
+import 'package:pasell/Models/Home/userwshome.dart';
+import 'package:pasell/Models/Product.dart';
+import 'package:http/http.dart' as http;
+import 'package:pasell/Controller/AuthController.dart';
+import 'package:pasell/Models/Product/FavoriteProduct.dart';
+import 'package:pasell/Models/Response/PurchasedProductsResponse.dart';
+import 'package:pasell/Models/ResponseModels.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class ProductController {
+  String server = baseServerUrl;
+  final secureStorage = FlutterSecureStorage();
+
+  Future<ResponseModels> addOrDeleteProductFavorite({String uidProduct}) async {
+    final token = await authController.readToken();
+    final resp = await http.post(Uri.parse('$server/add-Favorite-Product'),
+        headers: {
+          'Accept': 'application/json',
+          'xx-token': token
+        },
+        body: {
+          'uidProduct': uidProduct,
+          'uidUser': await secureStorage.read(key: 'uid')
+        });
+    print(resp.body);
+
+    return ResponseModels.fromJson(jsonDecode(resp.body));
+  }
+
+  Future<List<Favorite>> favoriteProducts() async {
+    final token = await authController.readToken();
+
+    final resp = await http.get(
+      Uri.parse('$server/product-favorite-for-user'),
+      headers: {'Accept': 'application/json', 'xx-token': token},
+    );
+
+    // print(resp.body);
+
+    var favs = FavoriteProduct.fromJson(jsonDecode(resp.body)).favorites;
+    print(favs);
+    return favs;
+  }
+
+  Future<ResponseModels> saveOrderBuyProduct(
+      {String receipt,
+      String date,
+      String amount,
+      List<ProductCart> products}) async {
+    final token = await authController.readToken();
+
+    Map<String, dynamic> data = {
+      'receipt': receipt,
+      'date': date,
+      'amount': amount,
+      'products': products
+    };
+
+    final body = json.encode(data);
+
+    final resp = await http.post(Uri.parse('$server/save-order-products'),
+        headers: {'Content-type': 'application/json', 'xx-token': token},
+        body: body);
+
+    return ResponseModels.fromJson(jsonDecode(resp.body));
+  }
+
+  Future<PurchasedProductsResponse> getPurchasedProducts() async {
+    final token = await authController.readToken();
+
+    final response = await http.get(
+      Uri.parse('$server/get-purchased-products'),
+      headers: {'Content-type': 'application/json', 'xx-token': token},
+    );
+
+    return PurchasedProductsResponse.fromJson(jsonDecode(response.body));
+  }
+
+  Future<List<Product>> getProductsForCategories({String id}) async {
+    final token = await authController.readToken();
+
+    final response = await http.get(
+      Uri.parse('$server/get-products-for-categories/' + id),
+      headers: {'Content-type': 'application/json', 'xx-token': token},
+    );
+
+    return ProductsHome.fromJson(jsonDecode(response.body)).products;
+  }
+
+  Future<List<Users>> getuserid({String id}) async {
+    // final token = await authController.readToken();
+
+    final response = await http.get(
+      Uri.parse('$server/getuserid/$id'),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    );
+
+    return UserHome.fromJson(jsonDecode(response.body)).user;
+  }
+
+  Future<List<Product>> getproductid({String id}) async {
+    // final token = await authController.readToken();
+
+    final response = await http.get(
+      Uri.parse('$server/getproductid/$id'),
+      headers: {'Content-type': 'application/json'},
+    );
+
+    return ProductsHome.fromJson(jsonDecode(response.body)).products;
+  }
+}
+
+final dbProductController = ProductController();
