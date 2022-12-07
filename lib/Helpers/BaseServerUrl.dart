e97a5fa1380fa4cb21e4d90ab27a24e303655834
78891RAM <raambhandari78891@gmail.com>
2022-12-07 22:22:22 +0545

    final helpers

--- lib/Helpers/BaseServerUrl.dart
+++ lib/Helpers/BaseServerUrl.dart
@@ -1,8 +1,3 @@
-// const baseServerUrl = "http://10.1.7.122:8471/api";
-// const publicServerUrl = "http://10.1.7.122:8471/";
-
-const baseServerUrl = "http://192.168.1.67:8471/api";
-const publicServerUrl = "http://192.168.1.67::8471/";
-
-// const baseServerUrl = "http://192.168.1.67:8471/api";
-// const publicServerUrl = "http://192.168.1.67:8471/";
+const baseServerUrl = "http://192.168.0.110:8471/api";
+const publicServerUrl = "http://192.168.0.110:8471/";
+// 192.168.191.98
---
+++ lib/Helpers/modalPayment.dart
@@ -0,0 +1,36 @@
+import '../Views/Home/HomePage.dart';
+import '../Widgets/AnimationRoute.dart';
+import '../Widgets/CustomText.dart';
+import '../Widgets/CustomButton.dart';
+import 'package:flutter/material.dart';
+
+void modalPayment(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    barrierColor: Colors.white,
+    builder: (context) {
+      return AlertDialog(
+        elevation: 0,
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.check_circle_rounded, size: 100, color: Colors.green),
+            SizedBox(height: 10),
+            CustomText(
+              text: 'Successful Payment',
+              fontSize: 19,
+            ),
+            SizedBox(height: 20),
+            CustomButton(
+              text: 'Continue',
+              fontSize: 19,
+              onPressed: () => Navigator.of(context).pushReplacement(
+                  customRoute(page: HomePage(), curved: Curves.easeInOut)),
+            )
+          ],
+        ),
+      );
+    },
+  );
+}
