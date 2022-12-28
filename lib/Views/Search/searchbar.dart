2c13dc1f383153f845adf44512cb9f61037e17bd
Dipisha9538 <78410665+Dipisha9538@users.noreply.github.com>
2022-12-28 21:27:52 +0545

    search

---
+++ lib/Views/Search/searchbar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Container(
+                height: 42,
+                margin: const EdgeInsets.only(left: 18, top: 10, right: 18),
+                child: Material(
+                  borderRadius: BorderRadius.circular(7),
+                  elevation: 1,
+                  child: TextFormField(
+                    onFieldSubmitted: (value) {},
+                    decoration: InputDecoration(
+                      prefixIcon: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.only(left: 6),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                            size: 23,
+                          ),
+                        ),
+                      ),
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 240, 239, 239),
+                      contentPadding: const EdgeInsets.only(top: 10),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.black38,
+                          width: 1,
+                        ),
+                      ),
+                      hintText: 'Search',
+                      hintStyle: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
