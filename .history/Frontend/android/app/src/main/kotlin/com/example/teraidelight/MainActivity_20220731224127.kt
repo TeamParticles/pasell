4311a81dccb63f3ce150315fdf512a93f3be851b
Rohit Sah <60933162+RohitSha1@users.noreply.github.com>
2023-01-02 15:30:03 +0545

    history deletion for updates project

--- .history/Frontend/android/app/src/main/kotlin/com/example/teraidelight/MainActivity_20220731224127.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aimsdigital
-
-import io.flutter.embedding.android.FlutterActivity
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity(){
-
-}
