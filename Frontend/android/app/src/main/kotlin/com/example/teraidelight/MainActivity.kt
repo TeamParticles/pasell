e76277ef223b79143ecfdb4269024b664b546dc6
Rohit Sah <60933162+RohitSha1@users.noreply.github.com>
2023-01-02 15:28:40 +0545

    Update correction

--- Frontend/android/app/src/main/kotlin/com/example/teraidelight/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.teamparticles.pasell
-
-import io.flutter.embedding.android.FlutterActivity
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity(){
-
-}
--- Frontend/lib/Helpers/BaseServerUrl.dart
+++
@@ -1,5 +0,0 @@
-// const baseServerUrl = "http://10.1.7.122:8471/api";
-// const publicServerUrl = "http://10.1.7.122:8471/";
-
-const baseServerUrl = "http://192.168.1.2:8471/api";
-const publicServerUrl = "http://192.168.1.2:8471/";
